bbdce1f0fb811998b355940b33f723a3f062a0b0
Amal Jose <amaljose96@gmail.com>
2020-04-29 04:13:29 +0530

    Match List scrollable

--- apps/ui/src/SportsTips/Pages/Stats/MatchDetails/MatchList/styles.js
+++ apps/ui/src/SportsTips/Pages/Stats/MatchDetails/MatchList/styles.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
+import colors from "../../../../colors";
 
-export const MatchListContainer = styled.div``;
+export const MatchListContainer = styled.div`
+    min-height:calc(100vh - 350px);
+    max-height:calc(100vh - 350px);
+    overflow-y:scroll;
+    background-color:${colors.primary}09;
+`;
